070285b6d515833a61184802e1e6146917497d40
a2 <a2@192.168.1.131>
2020-09-10 18:12:09 +0900

    2020 09 10 퇴근전 커밋

--- src/main/java/egovframework/com/faq/controller/FaqController.java
+++ src/main/java/egovframework/com/faq/controller/FaqController.java
@@ -1,5 +1,21 @@
 package egovframework.com.faq.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/faq")
 public class FaqController {
+	protected Log log = LogFactory.getLog(FaqController.class);
+	
+	@RequestMapping(value="/faqListPage.do")
+	public String faqListPage(ModelMap model) throws Exception{
+		
+		
+		return "faq/faqList";
+	}
 
 }
